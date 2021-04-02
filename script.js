8f621b1dc585608977c20a8177f07b675819522b
Niyaz Gallyamov <nizik1995@yandex.ru>
2021-04-02 17:32:04 +0500

    Сделано задание 2урока

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 "use strict";
 
+let money = 50000;
+console.log(typeof(money + " Доход за месяц"));
+
+let income = "Фриланс";
+console.log(typeof(income));
+
+let addExpenses = "интернет, такси, коммуналка";
+console.log(addExpenses.length);
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(", "));
+
+
+let deposit = true;
+
+let mission = 500000;
+console.log("Цель заработать " + money + " рублей/долларов/гривен/юани");
+
+let period = 12;
+console.log(typeof (period));
+console.log("Период равен " + period + " месяцев");
+
+
+let budgetDay = (money / 30);
+console.log(budgetDay);
